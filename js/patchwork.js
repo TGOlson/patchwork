34b66b6b843d9e39dca7563c2fad5432a7248e61
TGOlson <tydotg@gmail.com>
2014-02-28 00:15:33 -0800

    formatting

--- js/patchwork.js
+++ js/patchwork.js
@@ -1,44 +1,71 @@
-(function (global, factory)  {
+/*
+ * Patchwork
+ * http://tgolson.com/patchwork/
+ *
+ * v 1.0.0
+ */
 
-  global.Patchwork = factory(global);
+(function ( global, factory )  {
 
-}( this, function() {
+  global.Patchwork = factory( global );
+
+}( this, function () {
 
   var $patchwork;
-
   var $parent;
-
   var attrs =  {};
 
-  function init() {
+
+  /*
+   * Initializer
+   */
+  function init () {
     definePatchwork();
-    setParent();
+    defineParent();
     checkForTargetSizeData();
     runSizingFunctions();
     $(window).resize( refresh );
   }
 
-  function definePatchwork() {
+
+  /*
+   * Define the patchwork area
+   */
+  function definePatchwork () {
     $patchwork = $('#patchwork');
     $patchwork.css('text-align', 'left');
   }
 
-  function setParent() {
+
+  /*
+   * Define the parent of the patchwork
+   */
+  function defineParent () {
     $parent = $patchwork.parent();
     $parent.css('overflow', 'hidden');
 
+    // Default to the window if none defined
     if ( $parent.html() == $('body').html() ) {
       $parent = $(window);
     }
-
   }
 
-  function checkForTargetSizeData() {
+
+  /*
+   * Check for user settings for target patch sizes
+   * Default to 100px
+   */
+  function checkForTargetSizeData () {
     attrs.targetPatchSizeY = $patchwork.data().targetSizeX || 100;
     attrs.targetPatchSizeX = $patchwork.data().targetSizeY || 100;
   }
 
-  function runSizingFunctions() {
+
+  /*
+   * Start the process of shaping patches
+   */
+  function runSizingFunctions () {
+    // Store the previous patch count for later comparisons
     var prevPatchCount = attrs.patchCountTotal;
 
     setAttrs();
@@ -46,19 +73,27 @@
     choosePatchFunction( prevPatchCount );
   }
 
-  function setAttrs() {
+
+  /*
+   * Set patchwork attributes
+   */
+  function setAttrs () {
 
     attrs.patchworkX = $parent.width();
     attrs.patchworkY = $parent.height();
 
+    // Round down for patchCounts
     attrs.patchCountX = Math.floor( attrs.patchworkX / attrs.targetPatchSizeX );
     attrs.patchCountY = Math.floor( attrs.patchworkY / attrs.targetPatchSizeY );
 
+    // Set minimum patches to one - zero would leave the screen blank
     attrs.patchCountX === 0 ? this.patchCountX = 1 : null;
     attrs.patchCountY === 0 ? this.patchCountY = 1 : null;
 
     var styleSets = $patchwork.data().styleSets;
 
+    // Check if the number of stylesets is a factor of the horizontal row count
+    // If it is, the patches would align in an ugly way
     if ( attrs.patchCountX % styleSets === 0 ) {
       attrs.patchCountX += 1;
       attrs.patchCountY += 1;
@@ -67,8 +102,9 @@
     attrs.patchSizeX = attrs.patchworkX / attrs.patchCountX;
     attrs.patchSizeY = attrs.patchworkY / attrs.patchCountY;
 
+    // Try to find a patchCount that is a factor of the total patchwork dimensions
     if ( this.patchSizeY % 1 !== 0 ) {
-      var nearestInt = (function(num, div) {
+      var nearestInt = ( function ( num, div ) {
 
         for ( var i = 0; i < div * 0.2; i++ ) {
 
@@ -82,9 +118,10 @@
         }
 
         return 0
-      }( attrs.patchworkY, attrs.patchCountY ));
+      })( attrs.patchworkY, attrs.patchCountY );
     }
 
+    // If there was no no factor close to the original count, just round up
     if (nearestInt != 0) {
       attrs.patchCountY = nearestInt;
       attrs.patchSizeY  = attrs.patchworkY / attrs.patchCountY;
@@ -95,29 +132,45 @@
     attrs.patchCountTotal = attrs.patchCountX * attrs.patchCountY;
   }
 
-  function setPatchworkDimensions() {
+
+  /*
+   * Set the dimensions of the patchwork
+   */
+  function setPatchworkDimensions () {
     $patchwork.width( Math.ceil( attrs.patchSizeX ) * attrs.patchCountX );
     $patchwork.height( attrs.patchSizeY * attrs.patchCountY );
   }
 
-  function choosePatchFunction( prevPatchCount ) {
+
+  /*
+   * Decide which updating function will work
+   * Either update sizes, or re-render all patches
+   */
+  function choosePatchFunction ( prevPatchCount ) {
 
     if ( prevPatchCount == attrs.patchCountTotal && $('.patch')[0] !== undefined ) {
-      console.log('updating patch sizes')
       updatePatchSizes();
     } else {
       insertPatches();
     }
   }
 
-  function updatePatchSizes() {
+
+  /*
+   * Update existing patch sizes
+   */
+  function updatePatchSizes () {
     var $patches = $('.patch');
 
     $patches.width(attrs.patchSizeX);
     $patches.height(attrs.patchSizeY);
   }
 
-  function insertPatches() {
+
+  /*
+   * Clear all patches and re-render
+   */
+  function insertPatches () {
     destroy();
 
     for( var i = 0; i < attrs.patchCountTotal; i++ ) {
@@ -126,23 +179,33 @@
     }
   }
 
-  function createPatch( patchNum ) {
+
+  /*
+   * Patch factory
+   */
+  function createPatch ( patchNum ) {
     var newPatch = $('<div>')
       .attr('id', 'patch' + patchNum)
       .attr('class', 'patch')
       .css('width', attrs.patchSizeX)
       .css('height', attrs.patchSizeY)
-      .css('display', 'inline-block')
+      .css('display', 'inline-block');
 
-    var span = $('<span class="white-space-remover">')
+    // Add span with a text element to fix inline white-space
+    var span = $('<span>')
+      .attr('class', 'white-space-remover')
       .css('visibility', 'hidden')
       .css('font-size', '0px')
-      .html('.')
+      .html('.');
 
-    return newPatch.html(span)
+    return newPatch.html(span);
   }
 
-  function set(property, newValue) {
+
+  /*
+   * Setter function
+   */
+  function set ( property, newValue ) {
 
     if ( attrs[property] === undefined ) {
       throw 'Unknown property - ' + property;
@@ -154,32 +217,56 @@
     refresh();
   }
 
-  function get(property) {
+
+  /*
+   * Getter function
+   */
+  function get ( property ) {
 
     if ( attrs[property] === undefined ) {
       return attrs;
     } else {
-      return attrs[property]
+      return attrs[property];
     }
   }
 
-  function refresh() {
+
+  /*
+   * Refresh the patchwork to current conditions
+   */
+  function refresh () {
     runSizingFunctions()
   }
 
-  function destroy() {
+
+  /*
+   * Clear patchwork of all patches
+   */
+  function destroy () {
     $patchwork.empty();
   }
 
   return  {
+    /*
+     * Init and destroy
+     */
     init: init,
+    destroy: destroy,
+
+    /*
+     * Refresh patchwork
+     */
+    refresh: refresh,
+
+    /*
+     * Getter and setter
+     */
     get: get,
     set: set,
-    refresh: refresh,
-    destroy: destroy
-  }
-}))
+  };
 
-$(function() {
-  Patchwork.init()
-})
+}));
+
+$(function () {
+  Patchwork.init();
+});
